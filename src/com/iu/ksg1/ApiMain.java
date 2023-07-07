94fbf8fef9133e7eaf73de0c65c8a0b507ae65a2
ksg0069 <ksg0069@naver.com>
2023-07-07 17:07:13 +0900

    7.7

---
+++ src/com/iu/ksg1/ApiMain.java
@@ -0,0 +1,19 @@
+package com.iu.ksg1;
+
+public class ApiMain {
+
+	public static void main(String[] args) {
+		//객체 생성 
+		Object object = new Object();
+		
+		boolean check = object.equals(null);
+		String str = object.toString();
+		
+		String st = "hello";
+		char ch = st.charAt(1);
+		
+		System.out.println(ch);
+		
+	}
+
+}
